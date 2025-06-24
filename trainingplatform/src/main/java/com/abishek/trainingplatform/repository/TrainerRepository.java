1a15295e0b6f46423b2da7c345aef407607ac099
Abishek <anatarajan@diligentsquad.com>
2025-06-24 23:54:58 +0530

    commit

--- trainingplatform/src/main/java/com/abishek/trainingplatform/repository/TrainerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.abishek.trainingplatform.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.abishek.trainingplatform.model.Trainer;
-
-public interface TrainerRepository extends JpaRepository<Trainer, Long> {}
